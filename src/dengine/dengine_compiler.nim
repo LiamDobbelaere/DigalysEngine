0513c29e10bbe2e363502cec926f240aadb42ddf
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-09-18 20:40:37 +0200

    WIP labels

--- src/dengine/dengine_compiler.nim
+++ src/dengine/dengine_compiler.nim
@@ -1,14 +1,16 @@
 import dengine_types
 import dengine_utils
 import strutils
+import tables
 
 type
   DEngineCompiler* = ref object
     ## Compiles DEN code to DENB (bytecode that can be executed by the runtime directly)
+    labels: Table[string, int]
 
 proc init*(self: DEngineCompiler) =
   ## Initialize DEngineCompiler
-  discard
+  self.labels = initTable[string, int]()
 
 proc parseNonOpcode(self: DEngineCompiler, instruction: string): seq[uint8] =
   if instruction.contains("."):
@@ -27,6 +29,10 @@ proc compileInstruction(self: DEngineCompiler, instruction: string): seq[uint8] 
   if instruction.validOpcode:
     let opcodeByte = uint8(instruction.toOpcode())
     result.add(opcodeByte)
+  elif instruction.endsWith(":"):
+    self.labels[instruction.replace(":", "")] = 0
+    # should be result.length - 1, but you'll have to pass the entire program to here instead of making a new sequence,
+    # maybe just make the compiled program a property of self instead
   else:
     # Assume this is a constant or variable reference being pushed
     result.add(self.parseNonOpcode(instruction))
